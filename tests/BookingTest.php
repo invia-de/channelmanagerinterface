509228dc0e2408212eb3208bcbf3d53e06d9b82a
Torsten Burschka <torsten.burschka@invia.de>
2018-03-16 13:45:52 +0100

    ULN-195 - allow empty updatedDateTime - added possible error codes - code cleanup

--- tests/BookingTest.php
+++ tests/BookingTest.php
@@ -6,7 +6,6 @@ use Invia\CMI\BookedRate;
 use Invia\CMI\Booking;
 use Invia\CMI\ConstantsInterface;
 use Invia\CMI\Customer;
-use Invia\CMI\Person;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -46,8 +45,11 @@ class BookingTest extends TestCase
         $this->assertEquals($createdDateTime, $instance->getCreatedDateTime());
 
         $updatedDateTime = new \DateTime();
+        $this->assertNull($instance->getUpdatedDateTime());
         $this->assertEquals($instance, $instance->setUpdatedDateTime($updatedDateTime));
         $this->assertEquals($updatedDateTime, $instance->getUpdatedDateTime());
+        $this->assertEquals($instance, $instance->setUpdatedDateTime(null));
+        $this->assertNull($instance->getUpdatedDateTime());
 
         $bookedRates = [new BookedRate()];
         $this->assertEquals($instance, $instance->setBookedRates($bookedRates));
